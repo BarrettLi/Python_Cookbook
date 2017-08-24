9432d40cab8b3c53f886c20fc3dbb70f785d1972
Yuhai Li <liyuhai6666@gmail.com>
2017-08-23 18:31:00 -0700

    add 1.3 and 1.4

---
+++ Chapter-1/1.04.py
@@ -0,0 +1,27 @@
+# Problem: You want to make a list of the largest or smallest N items in a collection.
+
+# Solution: heapq 
+
+# ex1
+import heapq
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
+
+# ex2
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+
+# note: heapq is an approptiate method if you are looking for the N smallest/largest elements and N is small
+# compared to the overall size of the collection. If N is 1, then max() and min() are faster. If N is close
+# to the overall size, then it is faster to sort it first and take a slice.
