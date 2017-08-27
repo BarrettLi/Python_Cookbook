a1c7e081564939d16548f0dee605320a7d5b63ac
Yuhai Li <liyuhai6666@gmail.com>
2017-08-26 23:42:03 -0700

    1.18

---
+++ Chapter-1/1.18.py
@@ -0,0 +1,5 @@
+# Problem: You have code that accesses list or tuple elements by position, but this makes the code 
+# somewhat difficult to read at times. You’d also like to be less dependent on position in the 
+# structure, by accessing the elements by name.
+
+# Solution: collections.namedtuple()
