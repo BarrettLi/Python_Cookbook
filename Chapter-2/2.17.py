767bc0bda7714b334d648a99a616625ceb3b05ff
Yuhai Li <liyuhai6666@gmail.com>
2017-09-03 17:23:22 -0700

    2.17

---
+++ Chapter-2/2.17.py
@@ -0,0 +1,5 @@
+# Problem: You want to replace HTML or XML entities such as &entity; or &#code; with their
+# corresponding text. Alternatively, you need to produce text, but escape certain characters
+# (e.g. <, >, or &)
+
+# 
