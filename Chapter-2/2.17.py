3edee3c629b22ea2db6e93f91f15e8b0f1e470f6
Yuhai Li <liyuhai6666@gmail.com>
2017-09-22 14:50:54 -0700

    2.17

--- Chapter-2/2.17.py
+++ Chapter-2/2.17.py
@@ -2,4 +2,26 @@
 # corresponding text. Alternatively, you need to produce text, but escape certain characters
 # (e.g. <, >, or &)
 
-# 
+s = 'Elements are written as "<tag>text</tag>".'
+import html
+print(s) # Elements are written as "<tag>text</tag>".
+print(html.escape(s)) # Elements are written as &quot;&lt;tag&gt;text&lt;/tag&gt;&quot;.
+
+# Disable escaping of quotes
+print(html.escape(s, quote=False)) # Elements are written as "&lt;tag&gt;text&lt;/tag&gt;".
+
+# if you're trying to emit text as ASCII and want to embed character code entities for non-ASCII characters
+s = "Spicy Jalapeño"
+s.encode('ascii', errors='xmlcharrefreplace')
+# b'Spicy Jalape&#241;o'
+
+
+s = 'Spicy &quot;Jalape&#241;o&quot.'
+from html.parser import HTMLParser
+p = HTMLParser()
+p.unescape(s) # 'Spicy "Jalapeño".'
+
+t = 'The prompt is &gt;&gt;&gt;'
+from xml.sax.saxutils import unescapse
+unescape(t)
+'The prompt is >>>'
